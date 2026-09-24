dffda5eefed6a283be2b75cd58a176b588f95348
Innes <innygrant@outlook.com>
2026-09-24 14:00:28 -0700

    Add funFacts array with personal facts

---
+++ funfacts.js
@@ -0,0 +1,6 @@
+const funFacts = [
+  "you can find me under the name Innygr on quite a few platforms",
+  "the thing I use as a logo, is my OC!",
+  "my OC is a Celestan, a fictional species of creatures",
+  "My name is pronounced Inn-es",
+  "My OC's name is NegInny(Neg-Inn-e), pretty close to my name eh?"]
